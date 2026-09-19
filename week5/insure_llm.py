4a36fdd55c3699434ce56863464ff19333c8a1d0
miloszkulesza <milosz.kulesza1@gmail.com>
2026-09-19 14:15:18 +0200

    knowledge base, script with simple rag assistant using knowledge base

---
+++ week5/insure_llm.py
@@ -0,0 +1,68 @@
+import os
+import glob
+from pathlib import Path
+import gradio as gr
+from openai import OpenAI
+
+MODEL = "qwen3.5:4b"
+openai = OpenAI(base_url='http://localhost:11434/v1', api_key='ollama')
+
+knowledge = {}
+filenames = glob.glob("knowledge-base/employees/*")
+
+for filename in filenames:
+    name = Path(filename).stem.split(' ')[-1]
+    with open(filename, "r", encoding="utf-8") as f:
+        knowledge[name.lower()] = f.read()
+
+print(knowledge["lancaster"])
+print('=====================================')
+
+filenames = glob.glob("knowledge-base/products/*")
+
+for filename in filenames:
+    name = Path(filename).stem
+    with open(filename, "r", encoding="utf-8") as f:
+        knowledge[name.lower()] = f.read()
+
+print(knowledge.keys())
+print('=====================================')
+
+SYSTEM_PREFIX = """
+You represent Insurellm, the Insurance Tech company.
+You are an expert in answering questions about Insurellm; its employees and its products.
+You are provided with additional context that might be relevant to the user's question.
+Give brief, accurate answers. If you don't know the answer, say so.
+
+Relevant context:
+"""
+
+def get_relevant_context(message):
+    text = ''.join(ch for ch in message if ch.isalpha() or ch.isspace())
+    words = text.lower().split()
+    return [knowledge[word] for word in words if word in knowledge]
+
+print(get_relevant_context("Who is lancaster?"))
+print('=====================================')
+print(get_relevant_context("Who is Lancaster and what is carllm?"))
+print('=====================================')
+
+def additional_context(message):
+    relevant_context = get_relevant_context(message)
+    if not relevant_context:
+        result = "There is no additional context relevant to the user's question."
+    else:
+        result = "The following additional context might be relevant in answering the user's question:\n\n"
+        result += "\n\n".join(relevant_context)
+    return result
+
+print(additional_context("Who is Alex Lancaster?"))
+print('=====================================')
+
+def chat(message, history):
+    system_message = SYSTEM_PREFIX + additional_context(message)
+    messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
+    response = openai.chat.completions.create(model=MODEL, messages=messages)
+    return response.choices[0].message.content
+
+view = gr.ChatInterface(chat).launch(inbrowser=True)
